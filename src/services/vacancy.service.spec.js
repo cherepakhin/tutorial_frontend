c20227380de53c9e8323474dd310ab22664e21e8
Черепахин Василий <vasi.che@gmail.com>
2025-04-21 10:34:21 +0500

    rename in test getStb -> httpStub

--- src/services/vacancy.service.spec.js
+++ src/services/vacancy.service.spec.js
@@ -14,42 +14,42 @@ describe('VacancyService', () => {
   });
 
   it('VacancyService.getAll called http get', () => {
-    const getStub = sandbox.stub(http, 'get');
+    const httpStub = sandbox.stub(http, 'get');
     VacancyService.getAll();
-    expect(getStub.calledOnceWith('/vacancy/')).toBe(true);
+    expect(httpStub.calledOnceWith('/vacancy/')).toBe(true);
   });
 
   it('VacancyService.get(N) called http get', () => {
-    const getStub = sandbox.stub(http, 'get');
+    const httpStub = sandbox.stub(http, 'get');
     let VACANCY_N = 1;
     VacancyService.get(VACANCY_N);
-    expect(getStub.calledOnceWith('/vacancy/1')).toBe(true);
+    expect(httpStub.calledOnceWith('/vacancy/1')).toBe(true);
   });
 
   it('VacancyService.findByTitle. Check service call.', () => {
-    const getStub = sandbox.stub(http, 'post');
+    const httpStub = sandbox.stub(http, 'post');
     VacancyService.findByTitle('Test');
-    expect(getStub.calledOnceWith('/vacancy/find')).toBe(true);
+    expect(httpStub.calledOnceWith('/vacancy/find')).toBe(true);
   });
 
   it('VacancyService.create', () => {
-    const postStub = sandbox.stub(http, 'post');
+    const httpStub = sandbox.stub(http, 'post');
     const data = { title: 'Test', description: 'Test' };
     VacancyService.create(data);
-    expect(postStub.calledOnceWith('/vacancy/', data)).toBe(true);
+    expect(httpStub.calledOnceWith('/vacancy/', data)).toBe(true);
   });
 
   it('VacancyService.update', () => {
-    const postStub = sandbox.stub(http, 'post');
+    const httpStub = sandbox.stub(http, 'post');
     const data = { title: 'Test', description: 'Test' };
     VacancyService.update(1, data);
-    expect(postStub.calledOnceWith('/vacancy/1', data)).toBe(true);
+    expect(httpStub.calledOnceWith('/vacancy/1', data)).toBe(true);
   });
 
   it('VacancyService.delete(1)', () => {
-    const deleteStub = sandbox.stub(http, 'delete');
+    const httpStub = sandbox.stub(http, 'delete');
     VacancyService.delete(1);
-    expect(deleteStub.calledOnceWith('/vacancy/1')).toBe(true);
+    expect(httpStub.calledOnceWith('/vacancy/1')).toBe(true);
   });
 
   it('VacancyService.deleteAll()', () => {
