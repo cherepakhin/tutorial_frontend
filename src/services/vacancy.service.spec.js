25d4f25654582f400301c399be81f695d27bbcff
Черепахин Василий <vasi.che@gmail.com>
2025-04-22 19:14:04 +0500

    fix test 'should get all vacancies'

--- src/services/vacancy.service.spec.js
+++ src/services/vacancy.service.spec.js
@@ -5,30 +5,40 @@ import VacancyService from './vacancy.service';
 // Нужно запустить backend. Это !!!ИНТЕГАЦИОННЫЙ ТЕСТ!!!
 describe('VacancyService Integration test', () => {
   it('should get all vacancies', async () => {
-    const expectedData = [
-                         	{
-                         		"n": 2,
-                         		"title": "Vacancy 2 Company 1",
-                         		"comment": "",
-                         		"company": {
-                         		  "n": 1,
-                         		  "name": "Company 1",
-                         		},
-                         		"description": "Description Vacancy 2 Company 1",
-                         		"source": ""
-                         	},
-                         	{
-                         		"n": 3,
-                         		"title": "Vacancy 1 Company 2",
-                         		"comment": "",
-                         		"company": {
-                         		  "n": 2,
-                         		  "name": "Company 2",
-                         		},
-                         		"description": "Description Vacancy 1 Company 2",
-                         		"source": "",
-                         	},
-                         ];
+    const expectedData =  [
+                                 {
+                                   n: 1,
+                                   title: 'Vacancy 1 Company 1',
+                                   description: 'Description Vacancy 1 Company 1',
+                                   company: { n: 1, name: 'Company 1' },
+                                   source: '',
+                                   comment: ''
+                                 },
+                                 {
+                                   n: 2,
+                                   title: 'Vacancy 2 Company 1',
+                                   description: 'Description Vacancy 2 Company 1',
+                                   company: { n: 1, name: 'Company 1' },
+                                   source: '',
+                                   comment: ''
+                                 },
+                                 {
+                                   n: 3,
+                                   title: 'Vacancy 1 Company 2',
+                                   description: 'Description Vacancy 1 Company 2',
+                                   company: { n: 2, name: 'Company 2' },
+                                   source: '',
+                                   comment: ''
+                                 },
+                                 {
+                                   n: 4,
+                                   title: 'Vacancy 2 Company 2',
+                                   description: 'Description Vacancy 2 Company 2',
+                                   company: { n: 2, name: 'Company 2' },
+                                   source: '',
+                                   comment: ''
+                                 }
+                               ];
     const result = await VacancyService.getAll();
     console.log("--------------------");
     console.log(result.data);
