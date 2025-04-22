bbd3d1e0bbe4401c11438a6ce7b018c459bf63d6
Черепахин Василий <vasi.che@gmail.com>
2025-04-22 18:19:14 +0500

    vacancy.service.spec.js. Add "test should get all vacancies".

--- src/services/vacancy.service.spec.js
+++ src/services/vacancy.service.spec.js
@@ -16,34 +16,28 @@ describe('VacancyService', () => {
   it('should get all vacancies', async () => {
     const expectedData = [
                          	{
-                         		"n": 1,
-                         		"title": "Vacancy 1",
+                         		"n": 2,
+                         		"title": "Vacancy 2 Company 1",
                          		"comment": "",
                          		"company": {
                          		  "n": 1,
                          		  "name": "Company 1",
                          		},
                          		"description": "Description Vacancy 2 Company 1",
-                         		"n": 2,
-                         		"source": "",
-                         		"title": "Vacancy 2 Company 1",
+                         		"source": ""
                          	},
                          	{
-                         		"n": 2,
-                         		"title": "Vacancy 2",
+                         		"n": 3,
+                         		"title": "Vacancy 1 Company 2",
                          		"comment": "",
                          		"company": {
                          		  "n": 2,
                          		  "name": "Company 2",
                          		},
                          		"description": "Description Vacancy 1 Company 2",
-                         		"n": 3,
                          		"source": "",
-                         		"title": "Vacancy 1 Company 2",
                          	},
                          ];
-    mock.onGet('/vacancy/').reply(200, expectedData);
-
     const result = await VacancyService.getAll();
     console.log("--------------------");
     console.log(result.data);
@@ -51,14 +45,41 @@ describe('VacancyService', () => {
     expect(result.data).toEqual(expectedData);
   });
 
-//  it('should get a vacancy by id', async () => {
-//    const expectedData = { id: 1, title: 'Vacancy 1' };
-//    mock.onGet('/vacancy/1').reply(200, expectedData);
-//
-//    const result = await VacancyService.get(1);
-//    expect(result.data).toEqual(expectedData);
-//  });
-//
+  it('should get a vacancy by id', async () => {
+    const expectedData = {
+                             "n": 2,
+                             "title": "Vacancy 2 Company 1",
+                             "description": "Description Vacancy 2 Company 1",
+                             "comment": "",
+                             "source": "TODO",
+                             "company": {
+                                 "n": 1,
+                                 "name": "Company 1"
+                             }
+                         };
+    const result = await VacancyService.get(2);
+    expect(result.data).toEqual(expectedData);
+  });
+
+  it('should find vacancies by title', async () => {
+//    mock.onPost('/vacancy/find').reply(200, expectedData);
+
+    const result = await VacancyService.findByTitle('Vacancy 2 Company 1');
+    console.log(result);
+    const expectedData = [{
+                             "n": 2,
+                             "title": "Vacancy 2 Company 1",
+                             "description": "Description Vacancy 2 Company 1",
+                             "comment": "",
+                             "source": "",
+                             "company": {
+                                 "n": 1,
+                                 "name": "Company 1"
+                             }
+    }];
+    expect(result.data).toEqual(expectedData);
+  });
+
 //  it('should create a vacancy', async () => {
 //    const vacancyData = { title: 'New Vacancy' };
 //    const expectedData = { id: 3, title: 'New Vacancy' };
@@ -91,11 +112,4 @@ describe('VacancyService', () => {
 //    expect(result.status).toEqual(204);
 //  });
 
-//  it('should find vacancies by title', async () => {
-//    const expectedData = [{ id: 1, title: 'Vacancy 1' }, { id: 2, title: 'Vacancy 2' }];
-//    mock.onPost('/vacancy/find').reply(200, expectedData);
-//
-//    const result = await VacancyService.findByTitle('Vacancy');
-//    expect(result.data).toEqual(expectedData);
-//  });
 });
