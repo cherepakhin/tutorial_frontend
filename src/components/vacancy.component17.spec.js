700f093eb56fb72a0bf3f6705748c5d05b3403fb
Черепахин Василий <vasi.che@gmail.com>
2025-05-03 18:50:03 +0500

    add test test('find by ID (React17)'

--- src/components/vacancy.component17.spec.js
+++ src/components/vacancy.component17.spec.js
@@ -1,7 +1,7 @@
 // for React 17
 import React from 'react';
-import { render, screen } from '@testing-library/react';
-import { createRoot } from 'react-dom/client';
+import { render, screen, document } from '@testing-library/react';
+import { createRoot, hydrateRoot } from 'react-dom/client';
 import '@testing-library/jest-dom'
 // Тут добавляется средство проверки ожиданий
 //import 'react-testing-library/extend-expect'
@@ -19,5 +19,22 @@ describe('tests vacancy.component FOR REACT 17', () => {
     const linkElement = screen.getByText(/Описание вакансии/i);
 
     expect(linkElement).toBeInTheDocument();
+  });
+
+  test('find by text (React17)', () => {
+    let router = {router:{params: {n: "1", name: "NAME_1", description: "DESCRIPTION_1", source: "SOURCE_1"}}};
+
+    render(<VacancyForTest {...router}/>);
+
+    expect(screen.getByText('Описание вакансии')).toBeInTheDocument();
+  });
+
+  test('find by ID (React17)', () => {
+    let router = {router:{params: {n: "1", name: "NAME_1", description: "DESCRIPTION_1", source: "SOURCE_1"}}};
+
+    let result = render(<VacancyForTest {...router}/>);
+
+    const title = result.container.querySelector('#id_title');
+    expect(title).toBeInTheDocument();
   });
 });
