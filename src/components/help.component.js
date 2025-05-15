582193cb84e92b56242814410b006d46805f2523
Черепахин Василий <vasi.che@gmail.com>
2025-05-15 23:44:39 +0500

    add constructor for HelpPage

--- src/components/help.component.js
+++ src/components/help.component.js
@@ -3,13 +3,18 @@ import { withRouter } from '../common/with-router';
 
 export class HelpPage extends Component {
 
+  constructor(props) {
+    super(props);
+    console.log("HelpPage");
+  }
+
   render() {
     return (
         <div>
            <h5>Страница с описанием программы</h5>
         </div>
         )
-    }
+  }
 }
 
 export {
